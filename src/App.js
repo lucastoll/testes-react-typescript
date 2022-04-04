25f2a80ac8675ffe05060119b90b8209f06c945e
lucastoll <lucastolotto@rocky,ag>
2022-04-04 19:08:23 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {HelloComEstado} from './components/ComEstado';
+import {HelloSemEstado} from './components/semEstado';
+
+export default function App() {
+  return (
+   <>
+   <HelloComEstado name="juninho" />
+   <HelloSemEstado name="juninho" />
+   </>
+  );
+}
+
+
